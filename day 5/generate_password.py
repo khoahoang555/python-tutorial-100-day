74c0602cda9d70f166e201b88746097e599d1a1b
khoa pham <khoa.pham@tpssoft.com>
2022-11-10 16:39:06 +0700

    upload source code day 7

---
+++ day 5/generate_password.py
@@ -0,0 +1,53 @@
+# Go to: https://replit.com/@appbrewery/password-generator-start?v=1
+#Password Generator Project
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+#Eazy Level - Order not randomised:
+#e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+# result = ""
+# for i in range(0, nr_letters):
+#     number_rand = random.randint(0, len(letters) - 1)
+#     result += letters[number_rand]
+
+# for i in range(0, nr_symbols):
+#     number_rand = random.randint(0, len(symbols) - 1)
+#     result += symbols[number_rand]
+
+# for i in range(0, nr_numbers):
+#     number_rand = random.randint(0, len(numbers) - 1)
+#     result += numbers[number_rand]
+
+# print(result)
+#Hard Level - Order of characters randomised:
+#e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
+result = []
+for i in range(0, nr_letters):
+    number_rand = random.randint(0, len(letters) - 1)
+    result.append(letters[number_rand])
+
+for i in range(0, nr_symbols):
+    number_rand = random.randint(0, len(symbols) - 1)
+    result.append(symbols[number_rand])
+
+for i in range(0, nr_numbers):
+    number_rand = random.randint(0, len(numbers) - 1)
+    result.append(numbers[number_rand])
+
+print(''.join([s for s in result]))
+for i in range(0, len(result)):
+    temp = ""
+    number_rand_1 = random.randint(0, len(result) - 1)
+    number_rand_2 = random.randint(0, len(result) - 1)
+    temp = result[number_rand_1]
+    result[number_rand_1] = result[number_rand_2]
+    result[number_rand_2] = temp
+
+print(''.join([s for s in result]))
---
+++ day 7 - hangman/main.py
@@ -0,0 +1,213 @@
+import random
+import os
+
+word_list = [
+'crypt', 
+'curacao', 
+'cycle', 
+'daiquiri', 
+'dirndl', 
+'disavow', 
+'dizzying', 
+'duplex', 
+'dwarves', 
+'embezzle', 
+'equip', 
+'espionage', 
+'euouae', 
+'exodus', 
+'faking', 
+'fishhook', 
+'fixable', 
+'fjord', 
+'flapjack', 
+'flopping', 
+'fluffiness', 
+'flyby', 
+'foxglove', 
+'frazzled', 
+'frizzled', 
+'fuchsia', 
+'funny', 
+'gabby', 
+'galaxy', 
+'galvanize', 
+'gazebo', 
+'giaour', 
+'gizmo', 
+'glowworm', 
+'glyph', 
+'gnarly', 
+'gnostic', 
+'gossip', 
+'grogginess', 
+'haiku', 
+'haphazard', 
+'hyphen', 
+'iatrogenic', 
+'icebox', 
+'injury', 
+'ivory', 
+'ivy', 
+'jackpot', 
+'jaundice', 
+'jawbreaker', 
+'jaywalk', 
+'jazziest', 
+'jazzy', 
+'jelly', 
+'jigsaw', 
+'jinx', 
+'jiujitsu', 
+'jockey', 
+'jogging', 
+'joking', 
+'jovial', 
+'joyful', 
+'juicy', 
+'jukebox', 
+'jumbo', 
+'kayak', 
+'kazoo', 
+'keyhole', 
+'khaki', 
+'kilobyte', 
+'kiosk', 
+'kitsch', 
+'kiwifruit', 
+'klutz', 
+'knapsack', 
+'larynx', 
+'lengths', 
+'lucky', 
+'luxury', 
+'lymph', 
+'marquis', 
+'matrix', 
+'megahertz', 
+'microwave', 
+'mnemonic', 
+'mystify', 
+'naphtha', 
+'nightclub', 
+'nowadays', 
+'numbskull']
+
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+logo = ''' 
+ _                                             
+| |                                            
+| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
+| '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+| | | | (_| | | | | (_| | | | | | | (_| | | | |
+|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                    __/ |                      
+                   |___/    '''
+first_stage = True
+random_number = random.randint(0, len(word_list) - 1)
+select_question = word_list[random_number]
+
+check_lose = 1
+check_win = 0
+result = ["_" for char in select_question]
+word_your_guess = ""
+
+
+while True:
+    os.system('clear')
+    if first_stage == False:
+        check_stage = False
+        if word_your_guess in result:
+           print(f"You've already guessed {word_your_guess}")
+           check_stage = True
+          
+        for i in range(0, len(select_question)):
+            if select_question[i] == word_your_guess and word_your_guess not in result:
+                result[i] = select_question[i]
+                check_stage = True
+                check_win += 1 
+          
+        print("".join(result))
+      
+        if check_stage == False:
+            check_lose += 1
+            print(f"Your guessed {word_your_guess}, that's not in the word. You lose a life.")       
+
+        print(stages[7 - check_lose])
+      
+        if check_lose == 7:
+            print("You lose")
+            break
+          
+        if check_win == len(select_question):
+            print("You win")
+            break     
+         
+    else:
+        print(logo)
+        first_stage = False
+    
+    word_your_guess = input("Guess a letter: ")
+    
+   
+
+
+
+
+
